8f84fe81b16f521cc497ea4c1d560c7a4fac9387
Lurssed Daniel <dankimme16@gmail.com>
2023-03-17 02:51:01 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+    int i;
+    for (i = 25; i >= 0; i--) {
+        putchar('a' + i);
+    }
+    putchar('\n');
+
+    return 0;
+}
+
