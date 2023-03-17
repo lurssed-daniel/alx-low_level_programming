8ecad47fc7d900349a44c027f0fd573ac962d36c
Lurssed Daniel <dankimme16@gmail.com>
2023-03-17 06:37:17 -0700

    When I was having that alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,17 +5,19 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
+int main(void)
+{
     char c;
 
-    for (c = 'a'; c <= 'z'; ++c) {
-        if (c != 'e' && c != 'q') {
+    for (c = 'a'; c <= 'z'; ++c)
+    {
+        if (c != 'e' && c != 'q')
+        {
             putchar(c);
         }
     }
 
     putchar('\n');
 
-    return 0;
+    return (0);
 }
-
