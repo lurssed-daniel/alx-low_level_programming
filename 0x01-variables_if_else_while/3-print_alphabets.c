b34ca0bb6771b97a3ff67bd3cb8a3e19304c1e9a
Lurssed Daniel <dankimme16@gmail.com>
2023-03-17 02:33:59 -0700

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+    char c;
+
+    // print lowercase alphabet
+    for (c = 'a'; c <= 'z'; ++c) {
+        putchar(c);
+    }
+
+    // print uppercase alphabet
+    for (c = 'A'; c <= 'Z'; ++c) {
+        putchar(c);
+    }
+
+    // print newline character
+    putchar('\n');
+
+    return 0;
+}
+
