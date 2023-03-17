da22578a628bc492e4e9eae52fbeb9cb751c3ad6
Lurssed Daniel <dankimme16@gmail.com>
2023-03-17 02:47:56 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+    putchar('0');
+    putchar('\n');
+
+    putchar('1');
+    putchar('2');
+    putchar('3');
+    putchar('4');
+    putchar('5');
+    putchar('6');
+    putchar('7');
+    putchar('8');
+    putchar('9');
+    putchar('\n');
+
+    return 0;
+}
+
