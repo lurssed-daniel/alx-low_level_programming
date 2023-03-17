2c125e52aa29c1151ef1580402f3370eb0ad24d4
Lurssed Daniel <dankimme16@gmail.com>
2023-03-17 11:15:06 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,19 +5,15 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
-    putchar('0');
-    putchar('\n');
+int main(void)
+{
+    int i;
 
-    putchar('1');
-    putchar('2');
-    putchar('3');
-    putchar('4');
-    putchar('5');
-    putchar('6');
-    putchar('7');
-    putchar('8');
-    putchar('9');
+    for (i = 0; i < 10; i++)
+    {
+        putchar(i + '0');
+    }
+
     putchar('\n');
 
     return 0;
