4e8e0ed4a979eb861ba563a6f37abf757b92352f
Lurssed Daniel <dankimme16@gmail.com>
2023-03-21 04:46:41 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -47,7 +47,7 @@ int main(void)
     putchar(',');
     putchar(' ');
     putchar(r + '0');
-    _putchar('\n');
+    putchar('\n');
     r = print_sign(0xff);
     putchar(',');
     putchar(' ');
