e01853644775a3eee8465f5cce6eace7771023ee
Lurssed Daniel <dankimme16@gmail.com>
2023-03-23 04:12:26 -0700

    Check for a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isdigit - checks for a digit (0 through 9)
  * @c: character to check
- *
  * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
@@ -17,4 +16,3 @@ int _isdigit(int c)
         return (0);
     }
 }
-
